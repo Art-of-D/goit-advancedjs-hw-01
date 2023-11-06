291ede50bd70c46e96298ca139860c99caac4fe9
Art-of-D <114071335+Art-of-D@users.noreply.github.com>
2023-11-06 12:32:27 +0000

    Deploying to gh-pages from @ Art-of-D/goit-advancedjs-hw-01@1f056f7a5c075b37eb1b4686c99251daa1404d7d 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-ec808ebb.js";/* empty css                      */import{P as r,l as o}from"./assets/vendor-78be7656.js";const a=document.querySelector("iframe"),e=new r(a);e.on("timeupdate",o(function(t){localStorage.setItem("videoplayer-current-time",JSON.stringify(t))},1e3));const{seconds:i=0}=JSON.parse(localStorage.getItem("videoplayer-current-time"))||{};e.setCurrentTime(i);
+//# sourceMappingURL=commonHelpers2.js.map
